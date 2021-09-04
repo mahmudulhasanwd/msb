41c225c15b68c7eb31a8d934649fc1c7dc17a00c
unknown <you@example.com>
2021-09-05 00:29:47 +0600

    babu

---
+++ 08.Arithmetic_Operators.js
@@ -0,0 +1,10 @@
+// Arithmetic Operators (+,-,*,/,%,**)
+
+var num1 = 10;
+var num2 = 2;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
